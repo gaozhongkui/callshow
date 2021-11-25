1800244b5212d5ffd4959da97d2a6bfe98691d12
gaozhongkui <gaozhongkui@meetstarinc.com>
2021-11-25 17:38:17 +0800

    修改：全屏webview的页面展示

--- app/src/main/java/com/epiphany/callshow/App.kt
+++ app/src/main/java/com/epiphany/callshow/App.kt
@@ -1,11 +1,59 @@
 package com.epiphany.callshow
 
+import android.app.ActivityManager
 import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.os.Process
+import android.webkit.WebView
 import com.chaquo.python.android.PyApplication
 
 class App : Application() {
     init {
         mApplication = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        patchWebView()
+    }
+
+    fun getProcessName(context: Context?): String? {
+        return if (Build.VERSION.SDK_INT >= 28) {
+            getProcessName()
+        } else {
+            getProcessNameLegacy(context)
+        }
+    }
+
+    private fun getProcessNameLegacy(context: Context?): String? {
+        if (context == null) return null
+        val manager = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val infoList = manager.runningAppProcesses
+        if (infoList != null && infoList.size > 0) {
+            for (processInfo in infoList) {
+                if (processInfo.pid == Process.myPid()) {
+                    return processInfo.processName
+                }
+            }
+        }
+        return null
+    }
+
+    private fun patchWebView() {
+        try {
+            //适配安卓P，如果WebView使用多进程，添加如下代码
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val processName: String = getProcessName(this)!!
+                val packageName = baseContext.packageName
+                // 填入应用自己的包名
+                if (packageName != processName) {
+                    WebView.setDataDirectorySuffix(processName)
+                }
+            }
+        } catch (e: Throwable) {
+
+        }
     }
 
     companion object {
--- app/src/main/java/com/epiphany/callshow/function/web/FullWebActivity.kt
+++ app/src/main/java/com/epiphany/callshow/function/web/FullWebActivity.kt
@@ -1,7 +1,12 @@
 package com.epiphany.callshow.function.web
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.util.Log
+import android.webkit.*
 import com.epiphany.callshow.R
 import com.epiphany.callshow.common.base.BaseActivity
 import com.epiphany.callshow.common.base.BaseViewModel
@@ -20,10 +25,91 @@ class FullWebActivity : BaseActivity<BaseViewModel, ActivityFullWebLayoutBinding
         url?.let {
             binding.webView.loadUrl(url)
         }
-        binding.webView.settings.javaScriptEnabled = true
+        initWebSetting()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebSetting() {
+        val mSettings: WebSettings = binding.webView.settings ?: return
+        // 网页内容的宽度是否可大于WebView控件的宽度
+        mSettings.loadWithOverviewMode = false
+        // 保存表单数据
+        mSettings.saveFormData = true
+        // 是否应该支持使用其屏幕缩放控件和手势缩放
+        mSettings.setSupportZoom(true)
+        mSettings.builtInZoomControls = true
+        mSettings.displayZoomControls = false
+        // 启动应用缓存
+        mSettings.setAppCacheEnabled(true)
+        // 设置缓存模式
+        mSettings.cacheMode = WebSettings.LOAD_DEFAULT
+        // setDefaultZoom  api19被弃用
+        // 设置此属性，可任意比例缩放。
+        mSettings.useWideViewPort = true
+        // 告诉WebView启用JavaScript执行。默认的是false。
+        mSettings.javaScriptEnabled = true
+        //  页面加载好以后，再放开图片
+        //mSettings.setBlockNetworkImage(false)
+        // 使用localStorage则必须打开
+        mSettings.domStorageEnabled = true
+        // 排版适应屏幕
+        mSettings.layoutAlgorithm = WebSettings.LayoutAlgorithm.NARROW_COLUMNS
+
+        mSettings.databaseEnabled = true
+        mSettings.allowFileAccess = true
+        mSettings.javaScriptCanOpenWindowsAutomatically = true
+        // WebView是否支持多个窗口。
+        mSettings.setSupportMultipleWindows(true);
+        // webview从5.0开始默认不允许混合模式,https中不能加载http资源,需要设置开启。
+        mSettings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        // 设置字体默认缩放大小(改变网页字体大小,setTextSize  api14被弃用)
+        //mSettings.setTextZoom(100)
+
+        // 缩放比例 1
+        binding.webView.setInitialScale(1)
+        binding.webView.webViewClient = NormalWebViewClient()
+        binding.webView.webChromeClient = NormalWebChromeClient()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.webView.removeAllViews()
+        binding.webView.destroy()
+    }
+
+    private inner class NormalWebViewClient : WebViewClient() {
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            Log.d(
+                TAG,
+                "onPageStarted() called with: view = $view, url = $url, favicon = $favicon"
+            )
+        }
+
+        override fun onReceivedSslError(
+            view: WebView?,
+            handler: SslErrorHandler?,
+            error: SslError?
+        ) {
+            super.onReceivedSslError(view, handler, error)
+            Log.d(
+                TAG,
+                "onReceivedSslError() called with: view = $view, handler = $handler, error = $error"
+            )
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            Log.d(TAG, "onPageFinished() called with: view = $view, url = $url")
+        }
+    }
+
+    private inner class NormalWebChromeClient : WebChromeClient() {
+
     }
 
     companion object {
+        private const val TAG = "FullWebActivity"
         private const val EXTRA_URL = "url"
         fun launchAct(activity: Context, url: String?) {
             val intent = Intent(activity, FullWebActivity::class.java)
