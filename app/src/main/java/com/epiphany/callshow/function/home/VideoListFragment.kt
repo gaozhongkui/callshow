7db0a8c9dd6f86a862c27df1241078beef934f61
gaozhongkui <zhongkuigao@gmail.com>
2021-11-26 00:32:40 +0800

    修改：点击的地址页面跳转

--- app/src/main/java/com/epiphany/callshow/function/home/VideoListFragment.kt
+++ app/src/main/java/com/epiphany/callshow/function/home/VideoListFragment.kt
@@ -9,6 +9,7 @@ import com.epiphany.callshow.common.base.BaseFragment
 import com.epiphany.callshow.common.utils.SystemInfo
 import com.epiphany.callshow.constant.DEFAULT_PLAY_LIST_ID
 import com.epiphany.callshow.databinding.FragmentVideoListLayoutBinding
+import com.epiphany.callshow.function.web.FullWebActivity
 
 /**
  * 视频列表页面
@@ -92,13 +93,7 @@ class VideoListFragment : BaseFragment<VideoListViewModel, FragmentVideoListLayo
                     return
                 }
                 mVideoAdapter?.apply {
-                  /*  VideoDetailsActivity.launchActivity(
-                        activity!!,
-                        position,
-                        getDataList(),
-                        mPlayListId!!,
-                       ""
-                    )*/
+                    FullWebActivity.launchAct(requireActivity(), getDataList()[position].videoUrl)
                 }
             }
 
