93bd4a6226448c1768b616075756b2c2ac37de72
gaozhongkui <gaozhongkui@meetstarinc.com>
2021-11-25 11:26:22 +0800

    修改：不能运行的问题

--- app/src/main/java/com/epiphany/callshow/function/main/MainActivity.kt
+++ app/src/main/java/com/epiphany/callshow/function/main/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.epiphany.callshow.function.main
 
-import android.annotation.SuppressLint
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import com.epiphany.callshow.R
 import com.epiphany.callshow.common.base.BaseActivity
 import com.epiphany.callshow.common.base.BaseViewModel
@@ -10,11 +7,6 @@ import com.epiphany.callshow.databinding.ActivityMainBinding
 import com.epiphany.callshow.function.home.HomeFragment
 
 class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
-    //页面显示的所有Fragment
-    private val mHomFragments = mutableMapOf<Int, Fragment>()
-
-    //Tab的最后一个展示的序号
-    private var mTabLastIndex = -1
 
     override fun getBindLayout(): Int = R.layout.activity_main
 
@@ -27,82 +19,14 @@ class MainActivity : BaseActivity<BaseViewModel, ActivityMainBinding>() {
     }
 
     private fun initLayout() {
-        binding.llBottomBar.itemIconTintList = null
-        binding.llBottomBar.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> setFragmentPosition(TAB_HOME)
-//                R.id.my -> setFragmentPosition(TAB_PERSONAL)
-                R.id.video -> setFragmentPosition(TAB_VIDEO)
-            }
-            true
-        }
-        //默认选中的TAB
-        setFragmentPosition(TAB_HOME)
+        showHomeFragment()
     }
 
-    /**
-     * 设置页面的布局展示
-     */
-    @SuppressLint("ResourceType")
-    private fun setFragmentPosition(position: Int) {
-
-        //判断如果相同时，则直接返回
-        if (mTabLastIndex == position) {
-            return
-        }
-
-        //判断是否为视频Tab，如果是则设置不同的颜色
-        if (position == TAB_VIDEO) {
-            binding.llBottomBar.itemTextColor =
-                resources.getColorStateList(R.drawable.text_home_video_tab_title, theme)
-        } else {
-            binding.llBottomBar.itemTextColor =
-                resources.getColorStateList(R.drawable.text_home_tab_title, theme)
-        }
-
-        var fragment = mHomFragments[position]
-        val lastFragment = mHomFragments[mTabLastIndex]
-        val beginTransaction = supportFragmentManager.beginTransaction()
-        //判断对象为空时，则创建对象
-        if (fragment == null) {
-            fragment = when (position) {
-                TAB_HOME -> HomeFragment.newInstance()
-                TAB_VIDEO -> VideoDisplayFragment.newInstance()
-                else -> PersonalFragment.newInstance()
-            }
-            mHomFragments[position] = fragment
-        }
-        //判断如果未添加时，则添加到FragmentManager中
-        if (!fragment.isAdded) {
-            beginTransaction.add(R.id.fl_content, fragment)
-        }
-        beginTransaction.show(fragment)
-        beginTransaction.commitAllowingStateLoss()
-
-        //判断如果上一次的不为空，则隐藏上一次的Fragment并调用onPause
-        if (lastFragment != null) {
-            beginTransaction.hide(lastFragment)
-        }
-        mTabLastIndex = position
-        //调用状态
-        val stateBeginTransaction = supportFragmentManager.beginTransaction()
-        stateBeginTransaction.setMaxLifecycle(fragment, Lifecycle.State.RESUMED)
-        if (lastFragment != null) {
-            stateBeginTransaction.setMaxLifecycle(lastFragment, Lifecycle.State.STARTED)
-        }
-        stateBeginTransaction.commit()
-
+    private fun showHomeFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_content, HomeFragment.newInstance())
+        transaction.commit()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mHomFragments.clear()
-    }
-
-    companion object {
-        private const val TAB_HOME = 0
-        private const val TAB_PERSONAL = 1
-        private const val TAB_VIDEO = 2
-    }
 
 }
