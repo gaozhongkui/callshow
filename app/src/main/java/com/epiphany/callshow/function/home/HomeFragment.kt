84bac0fa2a1a794a5dae44e304d5cfd488413e81
gaozhongkui <gaozhongkui@meetstarinc.com>
2022-01-14 10:46:36 +0800

    修改：主页中Search的动画效果

--- app/src/main/java/com/epiphany/callshow/function/home/HomeFragment.kt
+++ app/src/main/java/com/epiphany/callshow/function/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.epiphany.callshow.function.home
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.epiphany.callshow.R
 import com.epiphany.callshow.common.base.BaseFragment
 import com.epiphany.callshow.common.utils.StatusBarUtil
 import com.epiphany.callshow.common.utils.SystemInfo
 import com.epiphany.callshow.databinding.FragmentHomeBinding
 import com.epiphany.callshow.function.search.SearchVideoActivity
+import kotlin.random.Random
 
 class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
     private var mHomePageAdapter: HomePageAdapter? = null
@@ -15,6 +20,14 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
 
     override fun getViewModelClass(): Class<HomeViewModel> {
         return HomeViewModel::class.java
+    }
+
+    private val mHandler = Handler(Looper.getMainLooper()) {
+        if (it.what == MSG_PLAY_SEARCH_ANIM) {
+            binding.ivSearch.playAnimation()
+        }
+
+        false
     }
 
     override fun initView() {
@@ -30,6 +43,13 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
         binding.ivSearch.setOnClickListener {
             SearchVideoActivity.launch(requireContext())
         }
+        binding.ivSearch.addAnimatorListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                sendPlaySearchAnimMsg()
+            }
+        })
+        sendPlaySearchAnimMsg()
     }
 
     private fun initDataObserver() {
@@ -50,12 +70,26 @@ class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding>() {
         }
     }
 
+    /**
+     * 播放搜索动画
+     */
+    private fun sendPlaySearchAnimMsg() {
+        if (isDetached) {
+            return
+        }
+        mHandler.removeMessages(MSG_PLAY_SEARCH_ANIM)
+        //随机产品一个区间
+        mHandler.sendEmptyMessageDelayed(MSG_PLAY_SEARCH_ANIM, Random.nextLong(12000, 26000))
+    }
+
     override fun onDestroy() {
         super.onDestroy()
+        mHandler.removeCallbacksAndMessages(null)
         mHomePageAdapter = null
     }
 
     companion object {
+        private const val MSG_PLAY_SEARCH_ANIM = 1068
         fun newInstance(bundle: Bundle? = null): HomeFragment {
             val fragment = HomeFragment()
             fragment.arguments = bundle
